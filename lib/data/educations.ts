136bc91c0d02bd1b599c652e0f0da519982221da
Filippo Alotta <filippo.alotta@swipeapp.studio>
2026-01-18 14:04:19 +0100

    added education course

--- lib/data/educations.ts
+++ lib/data/educations.ts
@@ -1,6 +1,16 @@
 export const educations = [
   {
     id: '1',
+    university: 'Start2Impact',
+    course: 'Web Development Bootcamp',
+    startDate: '2023',
+    endDate: '2023',
+    description:
+      'Completed an intensive Web Development Bootcamp covering HTML, CSS, JavaScript, React, Node.js, and database management.',
+    websiteUrl: 'https://www.start2impact.it/',
+  },
+  {
+    id: '2',
     university: 'University of Roma Tre',
     course: "Bachelor's Degree in Electronic Engineering",
     startDate: '2025',
